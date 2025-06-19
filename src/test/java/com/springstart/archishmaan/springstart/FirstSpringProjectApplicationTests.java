2dca1cf9eaea44af3ab46e6ef39e40c6910b05cd
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-19 12:51:49 +0530

    setting up spring boot

---
+++ src/test/java/com/springstart/archishmaan/springstart/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.archishmaan.springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
