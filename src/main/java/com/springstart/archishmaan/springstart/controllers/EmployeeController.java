4bfbef5ed523621f2d28fe34bf0d519a05befceb
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-21 21:06:45 +0530

    implemented RestController GET for fetching employees

---
+++ src/main/java/com/springstart/archishmaan/springstart/controllers/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.springstart.archishmaan.springstart.controllers;
+
+
+//Operations
+//GET /employees
+//POST /employees
+//DELETE /employees/{id}
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping(path = "/employees")
+    public String getEmployees(){
+        return "Employees";
+    }
+}
